4ff258d6ad4c35e792f67b30910edd2345bf219c
beck <snryid@163.com>
2021-10-28 09:24:56 +0800

    dev0.0.2

---
+++ src/SignerException.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ *  @author: BeckYang
+ *  @email: snryid@163.com
+ */
+
+namespace LadderProject\Spapi;
+
+class SignerException extends \Exception
+{
+
+}
---
+++ src/Utils/Signer.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ *  @author: BeckYang
+ *  @email: snryid@163.com
+ */
+
+namespace LadderProject\Spapi\Utils;
+
+use LadderProject\Spapi\SignerException;
+
+class Signer
+{
+    public static function sign(array $Options)
+    {
+
+
+    }
+}
